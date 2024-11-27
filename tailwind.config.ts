1c531bfcee4d415f53da10b8b79572adff13cdd2
“nhaserey” <lengmeng239@gmail.com>
2024-11-27 23:35:01 +0700

    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        lamaSky: "#5ba5f5",
+        lamaSky: "#4caef5",
         lamaSkyLight: "#EDF9FD",
         lamaPurple: "#CFCEFF",
         lamaPurpleLight: "#F1F0FF",
